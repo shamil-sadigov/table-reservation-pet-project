688c281b801625ce17d00fe235422baa7bf5794a
Shamil Sadigov <sov.shamil@gmail.com>
2021-07-22 04:14:38 +0400

    Rename TableSize to NumberOfSeats

---
+++ src/Services/Reservation/Reservation.Domain/Tables/NumberOfSeats.cs
@@ -0,0 +1,60 @@
+#region
+
+using System.Collections.Generic;
+using BuildingBlocks.Domain.DomainRules;
+using BuildingBlocks.Domain.ValueObjects;
+using Reservation.Domain.Tables.DomainRules;
+
+#endregion
+
+namespace Reservation.Domain.Tables
+{
+    public sealed class NumberOfSeats : ValueObject
+    {
+        private readonly byte _numberOfSeats;
+
+        private NumberOfSeats(byte numberOfSeats)
+        {
+            _numberOfSeats = numberOfSeats;
+        }
+
+        public static Result<NumberOfSeats> TryCreate(byte numberOfSeats)
+        {
+            var rule = new TableMustHaveAtLeastOneSeat(numberOfSeats);
+
+            var result = rule.Check();
+
+            if (result.Failed)
+                return result.WithResponse<NumberOfSeats>(null);
+
+            return new NumberOfSeats(numberOfSeats);
+        }
+
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return _numberOfSeats;
+        }
+
+        public override string ToString()
+        {
+            return _numberOfSeats.ToString();
+        }
+
+        #region Equality operators
+
+        public static bool operator >(NumberOfSeats first, NumberOfSeats second)
+            => first._numberOfSeats > second._numberOfSeats;
+
+        public static bool operator <(NumberOfSeats first, NumberOfSeats second) 
+            => first._numberOfSeats < second._numberOfSeats;
+
+        public static bool operator >=(NumberOfSeats first, NumberOfSeats second) 
+            => first._numberOfSeats >= second._numberOfSeats;
+
+        public static bool operator <=(NumberOfSeats first, NumberOfSeats second) 
+            => first._numberOfSeats <= second._numberOfSeats;
+
+        #endregion
+    }
+}
