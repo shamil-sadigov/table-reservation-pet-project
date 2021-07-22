1b00bf8e0a3550e62d0ee7e1737297080dcd2cc4
Shamil Sadigov <sov.shamil@gmail.com>
2021-07-22 04:16:45 +0400

    Rename WorkingHours to RestaurantWokringHours and add some tests

---
+++ src/Services/Reservation/Reservation.Domain.Tests/RestaurantWorkingHoursTests.cs
@@ -0,0 +1,131 @@
+﻿#region
+
+using System;
+using FluentAssertions;
+using Reservation.Domain.Restaurants;
+using Xunit;
+using Xunit.Abstractions;
+
+#endregion
+
+namespace Reservation.Domain.Tests
+{
+    public class RestaurantWorkingHoursTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public RestaurantWorkingHoursTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Theory]
+        [InlineData(10, 00, 55, 00, "finishTime should not be greater that 23:59:59")]
+        [InlineData(03, 00, 22, 00, "startTime should be in range 06:00:00-23:59:59")]
+        [InlineData(18, 00, 10, 00, "startTime should not be greater than finishTime")]
+        public void Cannot_create_workingHours_when_startTime_or_finishTime_are_invalid(
+            int startHour,
+            int startMinutes,
+            int finishHour,
+            int finishMinutes,
+            string expectedErrorMessage)
+        {
+            // Arrange
+            var startTime = new TimeSpan(startHour, startMinutes, seconds: 00);
+            var finishTime = new TimeSpan(finishHour, finishMinutes, seconds: 00);
+            
+            // Act
+            var result = RestaurantWorkingHours.TryCreate(startTime, finishTime);
+
+            // Assert
+            result.ShouldFail();
+            result.Errors!.ShouldContain(expectedErrorMessage);
+        }
+
+
+        [Theory]
+        [InlineData(10, 00, 19, 00)]
+        [InlineData(06, 00, 22, 30)]
+        [InlineData(09, 00, 10, 00)]
+        public void Can_create_workingHours_when_startTime_or_finishTime_are_valid(
+            int startHour,
+            int startMinutes,
+            int finishHour,
+            int finishMinutes)
+        {
+            // Arrange
+            var startTime = new TimeSpan(startHour, startMinutes, 00);
+            var finishTime = new TimeSpan(finishHour, finishMinutes, 00);
+            
+            // Act
+            var result = RestaurantWorkingHours.TryCreate(startTime, finishTime);
+
+            // Assert
+            result.ShouldSucceed();
+        }
+        
+        [Theory]
+        [InlineData(09, 15)]
+        [InlineData(11, 30)]
+        [InlineData(13, 45)]
+        [InlineData(15, 45)]
+        [InlineData(18, 45)]
+        public void Specified_time_is_restaurant_working_time(int hours, int minutes)
+        {
+            // Arrange
+            var workingHours = CreateWorkingHours(fromHours: 09, toHours: 20);
+
+            var flintstonesDateTime = new DateTime(
+                year: 1,
+                month: 1, 
+                day: 1, 
+                hours, 
+                minutes, 
+                second: 0);
+
+            var timeOfDay = flintstonesDateTime.TimeOfDay;
+
+            // Act
+            workingHours.IsWorkingTime(timeOfDay)
+                .Should()
+                .BeTrue();
+        }
+
+        [Theory]
+        [InlineData(06, 15)]
+        [InlineData(08, 45)]
+        [InlineData(20, 05)]
+        [InlineData(23, 00)]
+        [InlineData(00, 30)]
+        public void Specified_time_is_not_working_time_of_a_restaurant(int hours, int minutes)
+        {
+            // Arrange
+            var workingHours = CreateWorkingHours(fromHours: 09, toHours: 20);
+
+            var flintstonesDateTime = new DateTime(
+                year: 1,
+                month: 1, 
+                day: 1, 
+                hours, 
+                minutes, 
+                second: 0);
+
+            var timeOfDay = flintstonesDateTime.TimeOfDay;
+
+            // Act
+            workingHours.IsWorkingTime(timeOfDay)
+                .Should()
+                .BeFalse();
+        }
+
+        
+        private static RestaurantWorkingHours CreateWorkingHours(int fromHours, int toHours)
+        {
+            var startTime = new TimeSpan(fromHours, 00, 00);
+            var finishTime = new TimeSpan(toHours, 00, 00);
+
+            var workingTime = RestaurantWorkingHours.TryCreate(startTime, finishTime).Value!;
+            return workingTime;
+        }
+    }
+}
--- src/Services/Reservation/Reservation.Domain/Restaurants/RestaurantWorkingHours.cs
+++ src/Services/Reservation/Reservation.Domain/Restaurants/RestaurantWorkingHours.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
 using BuildingBlocks.Domain.DomainRules;
 using BuildingBlocks.Domain.ValueObjects;
 using Reservation.Domain.Restaurants.DomainRules;
@@ -12,34 +13,44 @@ namespace Reservation.Domain.Restaurants
     /// <summary>
     ///     Time period during a day when restaurant is open
     /// </summary>
-    public sealed class WorkingHours : ValueObject
+    public sealed class RestaurantWorkingHours : ValueObject
     {
         public static readonly TimeSpan MaxTime = new(23, 59, 59);
         public static readonly TimeSpan MinTime = new(06, 00, 00);
 
-        private WorkingHours()
+        private RestaurantWorkingHours()
         {
         }
 
         private TimeSpan StartTime { get; init; }
         private TimeSpan FinishTime { get; init; }
 
-        public static Result<WorkingHours> TryCreate(TimeSpan startTime, TimeSpan finishTime)
+        public static Result<RestaurantWorkingHours> TryCreate(TimeSpan startTime, TimeSpan finishTime)
         {
             var rule = new RestaurantWorkingHourMustBeInAcceptableRange(startTime, finishTime);
 
             var result = rule.Check();
 
             if (!result.Succeeded)
-                return result.WithResponse<WorkingHours>(null);
+                return result.WithResponse<RestaurantWorkingHours>(null);
 
-            var workingHours = new WorkingHours
+            return new RestaurantWorkingHours
             {
                 StartTime = startTime,
                 FinishTime = finishTime
             };
+        }
 
-            return result.WithResponse(workingHours);
+        public bool IsWorkingTime(TimeSpan timeSpan)
+        {
+            return timeSpan < FinishTime 
+                && timeSpan > StartTime;
+        }
+        
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return StartTime;
+            yield return FinishTime;
         }
     }
 }
