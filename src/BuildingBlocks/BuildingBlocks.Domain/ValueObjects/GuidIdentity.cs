b54e7920e8246f31aecc39218e80e3df29ddef47
Shamil Sadigov <sov.shamil@gmail.com>
2021-07-30 16:57:54 +0400

    Delete Value property since it exists in SingleValueObject

--- src/BuildingBlocks/BuildingBlocks.Domain/ValueObjects/GuidIdentity.cs
+++ src/BuildingBlocks/BuildingBlocks.Domain/ValueObjects/GuidIdentity.cs
@@ -4,8 +4,6 @@ namespace BuildingBlocks.Domain.ValueObjects
 {
     public class GuidIdentity : SingleValueObject<Guid>
     {
-        public Guid Value { get; }
-
         public GuidIdentity(Guid value) : base(value)
         {
             
