0a46a12bb32d09132885401d244ae3873f13163d
Shamil Sadigov <sov.shamil@gmail.com>
2021-08-07 15:15:04 +0400

    Add restaurant seeder

---
+++ src/Services/Restaurants/Tests/Restaurants.Api.IntegrationTests/IDataSeeder.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Infrastructure.Contexts;
+
+namespace Restaurants.Api.IntegrationTests
+{
+    public interface IDataSeeder
+    {
+        void Seed(DbContext context);
+    }
+}
---
+++ src/Services/Restaurants/Tests/Restaurants.Api.IntegrationTests/RestaurantSeeder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Restaurants;
+using Restaurants.Infrastructure.Contexts;
+
+namespace Restaurants.Api.IntegrationTests
+{
+    public class RestaurantSeeder:IDataSeeder
+    {
+        private readonly Domain.Restaurants.Restaurant[] _restaurant;
+
+        public RestaurantSeeder(params Domain.Restaurants.Restaurant[] restaurant)
+        {
+            _restaurant = restaurant;
+        }
+        
+        public void Seed(DbContext context)
+        {
+            context.Set<Domain.Restaurants.Restaurant>().AddRange(_restaurant);
+            context.SaveChanges();
+        }
+    }
+}
